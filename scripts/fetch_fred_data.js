e22ff22c5821a96735e77b96c84c18826a58e59a
fcjmp <pankarter@gmail.com>
2024-06-08 11:54:03 +0200

    Update fetch_fred_data.js

--- scripts/fetch_fred_data.js
+++ scripts/fetch_fred_data.js
@@ -4,11 +4,10 @@ const path = require('path');
 require('dotenv').config();
 
 const seriesInfo = [
-    { id: 'UNRATE', description: 'Unemployment Rate', units: 'Percent', category: 'Relative', transformation: 'lin' },
-    { id: 'CPIAUCSL', description: 'Consumer Price Index for All Urban Consumers: All Items', units: 'Index 1982-1984=100', category: 'Index', transformation: 'lin' },
-    { id: 'FYGDP', description: 'Federal Debt: Total Public Debt as Percent of Gross Domestic Product', units: 'Percent of GDP', category: 'Relative', transformation: 'lin' }
+    { id: 'UNRATE', description: 'Unemployment Rate', units: 'Percent', category: 'Relative' },
+    { id: 'CPIAUCSL', description: 'Consumer Price Index for All Urban Consumers: All Items', units: 'Index 1982-1984=100', category: 'Index' },
+    { id: 'FYGDP', description: 'Federal Debt: Total Public Debt as Percent of Gross Domestic Product', units: 'Percent of GDP', category: 'Relative' }
 ];
-
 const apiKey = process.env.FRED_API_KEY;
 const retryDelay = 60000; // Retry timeout in milliseconds (60 seconds)
 const maxRetries = 3;
@@ -20,10 +19,15 @@ async function fetchSeriesData(series) {
         try {
             const response = await axios.get(url);
             const observations = response.data.observations;
-            const transformation = response.data.transformation || series.transformation; // Fallback to manually set transformation if not present
-            const csvData = `Date,Value,Units,Transformation\n${observations.map(obs => `${obs.date},${obs.value},${series.units},${transformation}`).join('\n')}`;
-            fs.writeFileSync(path.join(__dirname, '../data', `${series.id.toLowerCase()}.csv`), csvData);
-            console.log(`${series.id} data fetched and saved successfully.`);
+            const dataCSV = `Date,Value\n${observations.map(obs => `${obs.date},${obs.value}`).join('\n')}`;
+            const metadataCSV = `Units,Category\n${series.units},${series.category}`;
+
+            // Write data file
+            fs.writeFileSync(path.join(__dirname, '../data', `${series.id.toLowerCase()}.csv`), dataCSV);
+            // Write metadata file
+            fs.writeFileSync(path.join(__dirname, '../data', `${series.id.toLowerCase()}_metadata.csv`), metadataCSV);
+
+            console.log(`${series.id} data and metadata fetched and saved successfully.`);
             break; // Break the loop if data is fetched successfully
         } catch (error) {
             console.error(`Attempt ${attempt} failed for ${series.id}: ${error.message}`);
